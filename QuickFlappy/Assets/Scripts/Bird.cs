b9ace83ffc48efb53db856797c35f07b04571150
David Quintero <dquintero@dusd.net>
2022-12-19 13:53:27 -0800

    up

--- QuickFlappy/Assets/Scripts/Bird.cs
+++ QuickFlappy/Assets/Scripts/Bird.cs
@@ -9,11 +9,15 @@ public class Bird : MonoBehaviour
     private Rigidbody2D rb2d;
     private Animator anim;
 
+    public AudioClip flap;
+    AudioSource audioSource;
+
     // Start is called before the first frame update
     void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
+        audioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -26,6 +30,7 @@ public class Bird : MonoBehaviour
                 rb2d.velocity = Vector2.zero;
                 rb2d.AddForce(new Vector2(0, upForce));
                 anim.SetTrigger("Flap");
+                PlaySound(flap);
             }
         }
         
@@ -37,5 +42,9 @@ public class Bird : MonoBehaviour
         isDead = true;
         anim.SetTrigger("Die");
         GameControl.instance.BirdDied();
+    }
+    public void PlaySound(AudioClip clip)
+    {
+        audioSource.PlayOneShot(clip);
     }
 }
--- QuickFlappy/Assets/Scripts/GameControl.cs
+++ QuickFlappy/Assets/Scripts/GameControl.cs
@@ -4,14 +4,17 @@ using UnityEngine;
 using TMPro;
 using UnityEditorInternal;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameControl : MonoBehaviour
 {
     public static GameControl instance;
     public GameObject gameOverText;
+    public TMP_Text scoreText;
     public bool gameOver = false;
     public float scrollSpeed = -1.5f;
 
+    private int score = 0;
     // Start is called before the first frame update
     void Awake()
     {
@@ -34,7 +37,15 @@ public class GameControl : MonoBehaviour
         }
         
     }
-
+    public void BirdScored()
+    {
+        if(gameOver)
+        {
+            return;
+        }
+        score++;
+        scoreText.text = "Score: " + score.ToString();
+    }
     public void BirdDied()
     {
         gameOverText.SetActive(true);
